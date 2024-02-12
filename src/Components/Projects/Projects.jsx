0ca0269d81952a2c0e63e1fccd44ea13d7c08dd2
Badal Prasad Mehta <65506036+badal-hub@users.noreply.github.com>
2024-02-13 01:52:31 +0530

    Project added

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,25 +5,30 @@ import "./Projects.scss";
 import pro1 from "../../assets/gallery.jpg";
 import pro2 from "../../assets/pro2.png";
 import pro3 from "../../assets/pro3.jpg";
-import Modal from '@mui/material/Modal';
+import Modal from "@mui/material/Modal";
+import bj from "../../assets/bj.png";
 import Carousel from "react-multi-carousel";
+import digi from "../../assets/digi.png";
+import swiggy from "../../assets/swiggy.png";
 import { gsap } from "gsap";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
 const Projects = () => {
   const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
+  const handleOpen = (element) => {
+    window.open(element.link, "_blank");
+  };
   const handleClose = () => setOpen(false);
 
   const onEnter = ({ currentTarget }) => {
@@ -55,21 +60,24 @@ const Projects = () => {
   const projectArray = [
     {
       id: 1,
-      name: "Photo Gallery Application",
-      para: "A photo gallery application is a digital platform that organizes and displays images or videos. It enables users to easily browse, manage, and share their media files, offering features like categorization, search, and often supports customization for a personalized viewing experience.",
-      im: pro1,
+      name: "Buddha Journey Web app",
+      para: "Embark on a transformative journey with our app, seamlessly blending travel and self-discovery. Explore new places, meet like-minded individuals, and document your personal growth. Embrace the adventure, connect with others, and unlock the full potential of your journey.",
+      im: bj,
+      link: "http://www.buddhajourney.co.in",
     },
     {
       id: 2,
-      name: "React Chat Application",
-      para: "A React chat application is a real-time messaging platform developed using the React JavaScript library. It allows users to engage in instant communication, featuring features such as message sending, real-time updates, user authentication, and often supports multimedia sharing for a dynamic chatting experience.",
-      im: pro2,
+      name: "Digipank Website",
+      para: "Digipank developing innovative solutions for businesses. Specializing in custom software development, web and mobile applications, and cloud-based solutions. Committed to delivering high-quality products, optimizing efficiency, and driving digital transformation for clients worldwide.",
+      im: digi,
+      link: "http://www.digipank.com",
     },
     {
       id: 3,
-      name: "React Movie and series Application",
-      para: "Users can also select series and movies based on their preferred genre and they can also get the details about the specific series or movie. Create a section where the user can search for movies and series as well, users can search by the movie name or series name and they will get to see all the results related to that particular name.",
-      im: pro3,
+      name: "Swiggy Clone",
+      para: "Swiggy is India's largest online food item ordering & delivery chain, it also tops the chart of India's Unicorn startup lists. It's a Bangalore-based startup started in 2014, and as of now, it's expanded to more than 100 Indian cities.",
+      im: swiggy,
+      link: "https://foodlover-six.vercel.app/",
     },
   ];
   return (
@@ -93,7 +101,7 @@ const Projects = () => {
                       className="btn"
                       onMouseEnter={onEnter}
                       onMouseLeave={onLeave}
-                      onClick={handleOpen}
+                      onClick={() => handleOpen(element)}
                     >
                       View Project
                     </Button>
@@ -111,7 +119,7 @@ const Projects = () => {
           );
         })}
       </Carousel>
-      <Modal
+      {/* <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
@@ -125,7 +133,7 @@ const Projects = () => {
           In progress, expected completion soon
           </Typography>
         </Box>
-      </Modal>
+      </Modal> */}
     </Box>
   );
 };
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -7,18 +7,19 @@ import redux from "../../assets/redux.svg";
 import scss from "../../assets/scss.svg";
 import js from "../../assets/js.svg";
 import css from "../../assets/css.svg";
+import tailwind from "../../assets/tailwind.svg";
 import saga from "../../assets/saga.svg";
 import html from "../../assets/html.svg";
+import jest from "../../assets/jest.svg";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import "./Skills.scss";
 const Skills = () => {
   return (
     <Box className="skillBlock" id="skills">
-      <ContentHead text="Skills" />
       <Box className="skillComponent">
-
-      <SkillComponent />
+      <ContentHead text="Skills" />
+        <SkillComponent />
       </Box>
     </Box>
   );
@@ -34,47 +35,48 @@ const data = [
   { id: 6, img: html, text: "Html" },
   { id: 7, img: css, text: "CSS" },
   { id: 8, img: saga, text: "Saga" },
-  
+  { id: 9, img: tailwind, text: "Tailwind CSS" },
+  { id: 10, img: jest, text: "Jest" },
 ];
 
 const SkillComponent = () => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 6
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 6,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
-    <Box >
-    <Carousel responsive={responsive} infinite={true}>
-      {data.map((Element) => {
-        return (
-          <Box key={Element.id} className="imgAndText">
-            <CardMedia
-              component="img"
-              image={Element.img}
-              className="imgWidth"
-            ></CardMedia>
+    <Box>
+      <Carousel responsive={responsive} infinite={true}>
+        {data.map((Element) => {
+          return (
+            <Box key={Element.id} className="imgAndText">
+              <CardMedia
+                component="img"
+                image={Element.img}
+                className="imgWidth"
+              ></CardMedia>
 
-            <Typography variant="twentyNormal" sx={{ textTransform: "none" }}>
-              {Element.text}
-            </Typography>
-          </Box>
-        );
-      })}
+              <Typography variant="twentyNormal" sx={{ textTransform: "none" }}>
+                {Element.text}
+              </Typography>
+            </Box>
+          );
+        })}
       </Carousel>
     </Box>
   );
